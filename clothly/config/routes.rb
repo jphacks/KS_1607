c8e5ec53b53365326d5f4e785c364b2f3d19e68c
Daiki Nagaoka <Daiki@lneck.local>
2016-10-30 02:44:56 +0900

    Add moduled routes

--- clothly/config/routes.rb
+++ clothly/config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'knowledges#index'
+
+  scope module: :knowledges do
+    get '/latest' => 'sorts#latest'
+    get '/ranking' => 'sorts#ranking'
+    get '/recommend' => 'sorts#recommend'
+  end
+
   resources :knowledges
   resources :users, only: [:show, :edit, :update]
 end
