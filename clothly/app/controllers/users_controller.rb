8c72d8118870eba84a506aafc1d6d86fb2129de7
daijiromaeyama <daijiro@maeyamadaijirou-no-MacBook-Air.local>
2016-10-29 22:18:01 +0900

    add edit and update action to users_controller.rb

--- clothly/app/controllers/users_controller.rb
+++ clothly/app/controllers/users_controller.rb
@@ -1,12 +1,24 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: :show
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to root_path
   end
 
   private
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:nickname, :email)
   end
 
 end
